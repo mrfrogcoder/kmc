abbe747cdc1e4e6a371ef0ca0b36e9b08f30a1af
Van Aldrin Sarzate <vanaldrinsarzate@gmail.com>
2017-02-07 15:40:52 +0800

    changes

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+<title>KMC | XML converter </title>
 <meta charset="utf-8">
-<title>KMC PHP Exam</title>
+<meta name="author" content="Van Aldrin Sarzate">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/skeleton.css">
 </head>
 
 <body>
